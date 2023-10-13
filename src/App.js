cc62e0f557c64b2205dfc4611acdd3e3b4d70dcb
AkhiSharmin <akhisharmin100@gmail.com>
2023-10-14 04:08:49 +0600

    added button

--- src/App.js
+++ src/App.js
@@ -46,9 +46,11 @@ function Friend({ friend }) {
 
     {friend.balance < 0 && <p className="red">You own ${friend.name} {Math.abs(friend.balance)}$</p>}
 
-    {friend.balance > 0 && <p className="red">You own ${friend.name} {Math.abs(friend.balance)}$</p>}
+    {friend.balance > 0 && <p className="green">{friend.name} owes you{Math.abs(friend.balance)}$</p>}
 
-    {friend.balance === 0 && <p className="red">You own ${friend.name} {Math.abs(friend.balance)}$</p>}
+    {friend.balance === 0 && <p>You age ${friend.name} are even</p>}
+
+    <button className="button">Select</button>
 
   </li>
 }
